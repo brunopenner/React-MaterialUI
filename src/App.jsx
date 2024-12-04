1e810749a99ab498b8bf4dbc995db98ef3ed7f3d
Bruno Penner <116695999+brunopenner@users.noreply.github.com>
2024-12-04 09:24:02 +0800

    #683. The Rating Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,13 @@ import './App.css'
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import AlarmIcon from '@mui/icons-material/Alarm';
+import RatingDemo from './RatingDemo';
 
 function App() {
 
   return (
     <>
-      <Button variant="contained" onClick={() => alert("hi!")}>Contained</Button>
-      <Button variant="text">Contained</Button>
-      <Button variant="outlined">Contained</Button>
-      <Button size="large" color="success" variant="contained" onClick={() => alert("hi!")}>Contained</Button>
-      <Button color="error" variant="contained" onClick={() => alert("hi!")}>Contained</Button>
-
-      <IconButton color="secondary" aria-label="add an alarm">
-        <AlarmIcon />
-      </IconButton>
+      <RatingDemo />
     </>
   )
 }
